d697d60449a2cfb12b1e7a1fc5ad3516d7de1a98
Ethan <jansenethan06@gmail.com>
2020-03-19 07:01:52 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage-component";
 import { Link, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop-component";
+import Header from "./components/header/header-component";
 
 const HatsPage = () => (
   <div>
@@ -38,6 +39,7 @@ function App() {
   return (
     <div>
       {/* <HomePage></HomePage> */}
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/hats" component={HatsPage} />
       <Route exact path="/jackets" component={JacketsPage} />
--- src/components/collection-preview/collection-preview-component.jsx
+++ src/components/collection-preview/collection-preview-component.jsx
@@ -9,7 +9,11 @@ const CollectionPreview = ({ title, items }) => (
       {items
         .filter((item, index) => index < 4)
         .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+          <CollectionItem
+            className="collection-item"
+            key={id}
+            {...otherItemProps}
+          />
         ))}
     </div>
   </div>
