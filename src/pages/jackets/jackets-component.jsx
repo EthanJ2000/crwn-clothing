1218f1ea789a1908836a3dea1723bf08826198e6
Ethan Jansen <52162288+EthanJ2000@users.noreply.github.com>
2020-03-30 14:31:36 +0200

    Add files via upload

---
+++ src/pages/jackets/jackets-component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./jackets-styles.scss";
+import SHOP_DATA from "../shop/shop-data";
+import CollectionPreview from "../../components/collection-preview/collection-preview-component";
+
+class JacketsPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections
+          .filter(item => item.title === "Jackets")
+          .map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))}
+      </div>
+    );
+  }
+}
+
+export default JacketsPage;
